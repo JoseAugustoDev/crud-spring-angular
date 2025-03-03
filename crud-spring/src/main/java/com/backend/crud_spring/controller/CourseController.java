85903f9f005c7835521889b49a6c58264650240b
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-03-03 10:21:32 -0300

    Criação do modelo "Course" e do repository utilizando JPA

---
+++ crud-spring/src/main/java/com/backend/crud_spring/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.backend.crud_spring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.crud_spring.model.Course;
+import com.backend.crud_spring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+     private CourseRepository courseRepository;
+     
+     @GetMapping
+     public List<Course> list() {
+          return null;
+     }
+}
