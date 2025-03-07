c05a72316efb9eed2c1dc3e7c9addde1e67e234c
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-03-07 13:35:19 -0300

    Criação do metodo DELETE no backend

--- crud-spring/src/main/java/com/backend/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/backend/crud_spring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,8 +34,8 @@ public class CourseController {
      public ResponseEntity<Course> findById(@PathVariable Long id) {
 
           return courseRepository.findById(id)
-               .map(record -> ResponseEntity.ok().body(record))
-               .orElse(ResponseEntity.notFound().build());
+                    .map(record -> ResponseEntity.ok().body(record))
+                    .orElse(ResponseEntity.notFound().build());
 
      }
 
@@ -50,10 +51,20 @@ public class CourseController {
                          record.setName(course.getName());
                          record.setCategory(course.getCategory());
 
-                         Course updated = courseRepository.save(record); 
+                         Course updated = courseRepository.save(record);
 
                          return ResponseEntity.ok().body(updated);
                     })
                     .orElse(ResponseEntity.notFound().build());
+     }
+
+     @DeleteMapping("/{id}")
+     public ResponseEntity<Void> delete(@PathVariable Long id) {
+          return courseRepository.findById(id)
+               .map(record -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+               })
+               .orElse(ResponseEntity.notFound().build());
      }
 }
