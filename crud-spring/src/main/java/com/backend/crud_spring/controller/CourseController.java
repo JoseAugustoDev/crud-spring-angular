e0877a4c6700f0b45d6eee23978099368e1dabbd
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-03-08 00:01:14 -0300

    Adicionando validação no backend

--- crud-spring/src/main/java/com/backend/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/backend/crud_spring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,19 +12,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.crud_spring.model.Course;
 import com.backend.crud_spring.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
 
-     private CourseRepository courseRepository;
+     private final CourseRepository courseRepository;
 
      @GetMapping
      public List<Course> list() {
@@ -31,7 +37,7 @@ public class CourseController {
      }
 
      @GetMapping("/{id}")
-     public ResponseEntity<Course> findById(@PathVariable Long id) {
+     public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
 
           return courseRepository.findById(id)
                     .map(record -> ResponseEntity.ok().body(record))
@@ -40,8 +46,9 @@ public class CourseController {
      }
 
      @PostMapping
-     public ResponseEntity<Course> createCourse(@RequestBody Course course) {
-          return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+     @ResponseStatus(code = HttpStatus.CREATED)
+     public Course createCourse(@RequestBody @Valid Course course) {
+          return courseRepository.save(course);
      }
 
      @PutMapping("/{id}")
@@ -59,7 +66,7 @@ public class CourseController {
      }
 
      @DeleteMapping("/{id}")
-     public ResponseEntity<Void> delete(@PathVariable Long id) {
+     public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
           return courseRepository.findById(id)
                .map(_ -> {
                     courseRepository.deleteById(id);
--- crud-spring/src/main/java/com/backend/crud_spring/model/Course.java
+++ crud-spring/src/main/java/com/backend/crud_spring/model/Course.java
@@ -1,10 +1,15 @@
 package com.backend.crud_spring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -15,9 +20,15 @@ public class Course {
      @GeneratedValue(strategy = GenerationType.AUTO)
      private Long id;
      
-     @Column(length = 200, nullable = false)
+     @NotBlank
+     @NotNull
+     @Column(length = 50, nullable = false)
+     @Length(min = 5, max = 50)
      private String name;
      
-     @Column(length = 200, nullable = false)
+     @NotNull
+     @Pattern(regexp = "Back-end|Front-end")
+     @Column(length = 10, nullable = false)
+     @Length(max = 10)
      private String category;
 }
